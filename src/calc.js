5cbc0bf41df9bbfd49fd0135139b62f185a358cf
IT Akademeia <go@it-akademeia.jp>
2020-10-23 16:28:50 +0900

    add function triple

--- src/calc.js
+++ src/calc.js
@@ -1,4 +1,7 @@
-// 関数 double
+//  calc.js
 module.exports.double  = function(x){
   return x * 2;
+}
+module.exports.triple = function(x){
+  return x * 3;
 }
--- src/calc.test.js
+++ src/calc.test.js
@@ -3,3 +3,6 @@ const calc = require('./calc');
 test('double 2 to equal 4', () => {
   expect(calc.double(2)).toBe(4);
 });
+test('tripe 2 to equal 6', () => {
+  expect(calc.triple(2)).toBe(6);
+});
